4893ea5291d83e5746de4cfc667f6463db446a75
George <georgemclellan0@gmail.com>
2018-08-25 09:33:14 +1000

    Progress commit

---
+++ react-redux-client/src/view/JobSeekerAppliedMatchedDetails.jsx
@@ -0,0 +1,11 @@
+<div>
+    <tr>
+        <h4 class="ui image header"></h4>
+        <td><img src="/images/avatar2/small/lena.png" class="ui mini rounded image" /></td>
+        <td>George Webber</td>
+        <td><div class="roleName">
+            Web Developer Intern</div></td>
+        <td class="center aligned"> <i class="large green checkmark icon"></i></td>
+        <td class="center aligned"> <i class="large green checkmark icon"></i></td>
+    </tr>
+</div>
---
+++ react-redux-client/src/view/SideMenu.jsx
@@ -0,0 +1,16 @@
+<div class="four wide column">
+    <div class="ui vertical fluid tabular menu">
+        <a class=" active item">
+            Roles
+        </a>
+        <a class="item">
+            Account
+        </a>
+        <a class="item">
+            Search
+        </a>
+        <a class="item">
+            Applicants
+        </a>
+    </div>
+</div>
