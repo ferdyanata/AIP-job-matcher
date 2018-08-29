6ec5d2a94701b7e609d259faa9958affe905fdf6
George <georgemclellan0@gmail.com>
2018-08-29 20:08:13 +1000

    Made indentation consistent

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -7,26 +7,26 @@ import EmployerTalentSelection from './EmployerTalentSelection';
 //Based of https://github.com/le-kang/AIP2018/blob/master/week04/react-router-example/src/App.js
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <div class="ui grid">
-          <div class="four wide column">
-            <SideMenu/>
-          </div>
-          <div class="twelve wide stretched column">
-            <h1>Job Matcher</h1>
-            <Switch>
-              <Route path='/selection' component= {EmployerTalentSelection} />
-              <Route path='/:usertype/positions' component={AdvertisedPositions}/>
-              <Route path='/:usertype/job-info/:id' component = {JobInfo}/>
-              <Route render={() => <Redirect to="/selection"/>} />
-            </Switch>
-          </div>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <div class="ui grid">
+                    <div class="four wide column">
+                        <SideMenu />
+                    </div>
+                    <div class="twelve wide stretched column">
+                        <h1>Job Matcher</h1>
+                        <Switch>
+                            <Route path='/selection' component={EmployerTalentSelection} />
+                            <Route path='/:usertype/positions' component={AdvertisedPositions} />
+                            <Route path='/:usertype/job-info/:id' component={JobInfo} />
+                            <Route render={() => <Redirect to="/selection" />} />
+                        </Switch>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 
