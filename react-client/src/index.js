407ef6f794b68882d2f29b1fc3109198c1d9af07
Ferdy <ferdy.anata@gmail.com>
2018-09-19 17:43:33 +1000

    Fix routing issue in index.js

--- react-client/src/index.js
+++ react-client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import 'semantic-ui-css/semantic.min.css';
@@ -10,9 +10,9 @@ import history from './helpers/history'
 
 ReactDOM.render(
     <Provider store={store}>
-        <BrowserRouter history={history}>
+        <Router history={history}>
             <App />
-        </BrowserRouter>
+        </Router>
     </Provider>,
     document.getElementById('root'));
 registerServiceWorker();
