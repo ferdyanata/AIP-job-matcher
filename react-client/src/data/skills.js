c14aa647c2f02249508f20b7a99010c5bdcd78fc
George <georgemclellan0@gmail.com>
2018-09-24 20:24:52 +1000

    Add skills works on talent register

---
+++ react-client/src/data/skills.js
@@ -0,0 +1,14 @@
+export const skills = [
+    { key: 'angular', text: 'Angular', value: 'angular' },
+    { key: 'java', text: 'java', value: 'java' },
+    { key: 'c++', text: 'c++', value: 'c++' },
+    { key: 'c#', text: 'c#', value: 'c#' },
+    { key: 'html', text: 'HTML', value: 'html' },
+    { key: 'javascript', text: 'javascript', value: 'javascript' },
+    { key: 'node', text: 'NodeJS', value: 'node' },
+    { key: 'python', text: 'Python', value: 'python' },
+    { key: 'rails', text: 'Rails', value: 'rails' },
+    { key: 'react', text: 'React', value: 'react' },
+    { key: 'ui', text: 'UI Design', value: 'ui' },
+    { key: 'ux', text: 'User Experience', value: 'ux' },
+  ]
