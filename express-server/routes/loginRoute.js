0e94af77b1e95be4b5eef25ae0abcc0713571e11
Ferdy <ferdy.anata@gmail.com>
2018-09-28 00:13:26 +1000

    Login sessions now available for talent and employer

--- express-server/routes/loginRoute.js
+++ express-server/routes/loginRoute.js
@@ -7,6 +7,40 @@ const router = express.Router();
 const Talent = require('../models/talentModel.js');
 const Employer = require('../models/employerModel');
 
+router.post('/employer-login', function (req, res) {
+    const email = req.body.email;
+
+    Employer.findOne({
+        email: email
+    }, function (err, employer) {
+        if (err) throw err;
+
+        if (!employer) {
+            res.status(401).send({ success: false, msg: 'Authentication failed. User not found.' });
+        } else {
+            // check if password matches
+            employer.comparePassword(req.body.password, function (err, isMatch) {
+                if (isMatch && !err) {
+                    // if talent is found and password is right create a token
+                    // this is public information, so only sign in with id and email
+                    req.login(employer, { session: false }, (err) => {
+                        if (err) {
+                            res.send(err);
+                        } else {
+                            const token = jwt.sign({ email: email }, settings.secret);
+                            // return the information including token as JSON
+                            res.json({ employer, success: true, msg: 'Successfully logged in!', token: token });
+                        }
+                    });
+                } else {
+                    res.status(401).send({ success: false, msg: 'Authentication failed. Wrong password.' });
+                }
+            });
+        }
+    });
+});
+
+
 router.post('/talent-login', function (req, res) {
     const email = req.body.email;
 
@@ -26,11 +60,11 @@ router.post('/talent-login', function (req, res) {
                     req.login(talent, { session: false }, (err) => {
                         if (err) {
                             res.send(err);
+                        } else {
+                            const token = jwt.sign({ talentId: talent._id, email: email }, settings.secret);
+                            // return the information including token as JSON
+                            res.json({ talent, success: true, token: token });
                         }
-                        
-                        const token = jwt.sign({ talentId: talent._id, email: email }, settings.secret);
-                        // return the information including token as JSON
-                        res.json({ talent, success: true, token: token });
                     });
 
                 } else {
@@ -39,47 +73,6 @@ router.post('/talent-login', function (req, res) {
             });
         }
     });
-
-
 });
-
-router.post('/employer-login', function (req, res) {
-    const email = req.body.email;
-    Employer.findOne({
-        email: email
-    }, function (err, employer) {
-        if (err) throw err;
-
-        if (!employer) {
-            res.status(401).send({ success: false, msg: 'Authentication failed. User not found.' });
-        } else {
-            // check if password matches
-            employer.comparePassword(req.body.password, function (err, isMatch) {
-                if (isMatch && !err) {
-                    // if talent is found and password is right create a token
-                    const token = jwt.sign(talent.toJSON(), settings.secret);
-                    // return the information including token as JSON
-                    res.json({ success: true, token: 'JWT ' + token });
-                } else {
-                    res.status(401).send({ success: false, msg: 'Authentication failed. Wrong password.' });
-                }
-            });
-        }
-    });
-});
-
-getToken = function (headers) {
-    if (headers && headers.authorization) {
-        var parted = headers.authorization.split(' ');
-        if (parted.length === 2) {
-            return parted[1];
-        } else {
-            return null;
-        }
-    } else {
-        return null;
-    }
-};
-
 
 module.exports = router;
