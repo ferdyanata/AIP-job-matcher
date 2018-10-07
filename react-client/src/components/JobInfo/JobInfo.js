6455bf88171d6aaaab02bd675be7b62ee8995725
Ferdy <ferdy.anata@gmail.com>
2018-10-08 00:56:05 +1100

    Display desired skills in position list

--- react-client/src/components/JobInfo/JobInfo.js
+++ react-client/src/components/JobInfo/JobInfo.js
@@ -3,13 +3,27 @@ import Applicants from "../Applicants/Applicants";
 import PositionApplication from "../PositionApplication/PositionApplication";
 import { connect } from 'react-redux';
 import { fetchPosition } from '../../actions/positionActions';
+import { Header, Table } from 'semantic-ui-react'
 
 class JobInfo extends React.Component {
-
-
     componentWillMount() {
         this.props.fetchPosition(this.props.match.params.id);
     }
+
+    desiredSkillsList = () => {
+        var {desiredSkills} = this.props.position
+        let listArray = [desiredSkills]
+
+        for(let i = 0; i < 100; i++){
+            let children = []
+            for (let j = 0; j < listArray.length[i]; j++){
+                children.push(<Table.Cell>{desiredSkills}</Table.Cell>)
+            }
+            listArray.push(<Table.Row>{children}</Table.Row>)
+        }
+        return listArray
+    }
+
 
     render() {
         const usertype = localStorage.getItem('user_type');
@@ -17,13 +31,10 @@ class JobInfo extends React.Component {
             <div class="ui segment">
                 <h3>{this.props.position.positionName}</h3>
                 <p>{this.props.position.description}</p>
-                {/* Show skills here in a list
-                    Loop over the desiredSkills array 
-                    this.props.position.desiredSkills    
-                */}
-                <br/>
-                {usertype === 'employer' ? ( <Applicants />) : <PositionApplication position={this.props.position}/>}
-                                     
+                <p>{this.desiredSkillsList()}</p>
+                <br />
+                {usertype === 'employer' ? (<Applicants />) : <PositionApplication position={this.props.position} />}
+
             </div>
         );
     }
@@ -34,4 +45,4 @@ const mapStateToProps = state => ({
     position: state.positions.item
 });
 
-export default connect(mapStateToProps, {fetchPosition})(JobInfo);
+export default connect(mapStateToProps, { fetchPosition })(JobInfo);
